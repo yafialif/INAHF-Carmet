129c70437f7cfe255ecc3c66c2af9b9673b92569
Yafi Alif <yafialif01@gmail.com>
2023-09-21 14:37:38 +0700

    update exportadhf 2

--- app/Http/Controllers/Admin/ExportAdhfController.php
+++ app/Http/Controllers/Admin/ExportAdhfController.php
@@ -32,7 +32,7 @@ class ExportAdhfController extends Controller
 				->join('adhfoutcomes', 'patient.id', '=', 'adhfoutcomes.patient_id')
 				->join('adhfriskfactors', 'patient.id', '=', 'adhfriskfactors.patient_id')
 				->join('adhfhospitalization', 'patient.id', '=', 'adhfhospitalization.patient_id')
-				// ->join('clinicalprofile', 'patient.id', '=', 'clinicalprofile.user_id')
+				->join('clinicalprofile', 'patient.id', '=', 'clinicalprofile.user_id')
 				// ->join('adhfrothorax', 'patient.id', '=', 'adhfrothorax.patient_id') Hapus
 				->where('patient.categorytreatment_id', 1)
 				->get();
