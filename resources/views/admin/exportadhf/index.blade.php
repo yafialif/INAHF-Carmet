a0e751e51873c53c2d94483e5772e634c6e99538
Yafi Alif <yafialif01@gmail.com>
2023-09-21 13:58:35 +0700

    update exportadhf

--- resources/views/admin/exportadhf/index.blade.php
+++ resources/views/admin/exportadhf/index.blade.php
@@ -31,7 +31,7 @@
 
                         {{-- Clinical Profile --}}
 
-                        <td>height</td>
+                        {{-- <td>height</td>
                         <td>weight</td>
                         <td>bmi</td>
                         <td>Heart Rate</td>
@@ -46,10 +46,10 @@
                         <td>NYHA Class</td>
                         <td>type of acute HF</td>
                         <td>respiratory failure</td>
-                        <td>cardiogenic shock</td>
+                        <td>cardiogenic shock</td> --}}
 
                         {{-- risk Factor --}}
-
+{{-- 
                         <td>hypertension</td>
                         <td>diabetes or prediabetes</td>
                         <td>dislipidemia</td>
@@ -61,21 +61,21 @@
                         <td>history of hf</td>
                         <td>history of pci or cabg</td>
                         <td>historyof heart valve surgery</td>
-                        <td>omi or cad</td>
+                        <td>omi or cad</td> --}}
                         {{-- Etiology --}}
-                        <td>Precipitating Factors</td>
+                        {{-- <td>Precipitating Factors</td> --}}
                         
 
                         {{-- Echocardiography --}}
-                        <td>ef</td>
+                        {{-- <td>ef</td>
                         <td>tapse</td>
                         <td>edv</td>
                         <td>esv</td>
                         <td>sign mr</td>
                         <td>LV max index</td>
-                        <td>E/e' average</td>
+                        <td>E/e' average</td> --}}
                         {{-- Blood Laboratory Test --}}
-                        <td>hemoglobin</td>
+                        {{-- <td>hemoglobin</td>
                         <td>hematocrite</td>
                         <td>random blood glucose</td>
                         <td>HbA1C</td>
@@ -86,17 +86,17 @@
                         <td>serum creatinine</td>
                         <td>gfr</td>
                         <td>Serum Iron</td>
-                        <td>NT-ProBNP</td>
+                        <td>NT-ProBNP</td> --}}
                         {{-- Blood Gas Analysis --}}
-                        <td>pH</td>
+                        {{-- <td>pH</td>
                         <td>pco2</td>
                         <td>hco3</td>
                         <td>po2</td>
                         <td>lactate</td>
-                        <td>be</td>
+                        <td>be</td> --}}
                         {{-- Medication --}}
                        
-                        <td>acei</td>
+                        {{-- <td>acei</td>
                         <td>aceiDoseatPredischarge</td>
                         <td>arb</td>
                         <td>arbDoseatPredischarge</td>
@@ -106,24 +106,21 @@
                         <td>BetaBlockerDoseatPredischarge</td>
                         <td>LoopDiureticDoseatPredischarge</td>
                         <td>sglt2i</td>
-                        {{-- <td>sglt2iDoseatPredischarge</td> --}}
                         <td>ivabradineDoseatPredischarge</td>
                         <td>Tolvaptan</td>
                         <td>Statin</td>
                         <td>Inotropic</td>
-                        <td>Vasoconstrictor</td>
+                        <td>Vasoconstrictor</td> --}}
                         {{-- Hospitalization --}}
-                        <td>iccu</td>
+                        {{-- <td>iccu</td>
                         <td>ward</td>
                         <td>totalLoS</td>
                         <td>hospitalizationCost</td>
-                        {{-- Outcomes --}}
                         <td>inhospitalDeath</td>
                         <td>vulnerablePhaseDeath</td>
                         <td>vulnerablePhaseRehospitalization</td>
                         <td>dateofDeath</td>
-                        <td>additional notes</td>
-                        {{-- <td>deleted at</td> --}}
+                        <td>additional notes</td> --}}
                     </tr>
                 </thead>
                 <tbody>
@@ -143,7 +140,7 @@
                         <td>{{ $row->education }}</td>
                         <td>{{ $row->gender }}</td>
                         {{-- Clinical Profile --}}
-                        <td>{{ $row->height }}</td>
+                        {{-- <td>{{ $row->height }}</td>
                         <td>{{ $row->weight }}</td>
                         <td>{{ $row->bmi }}</td>
                         <td>{{ $row->hr }}</td>
@@ -158,9 +155,9 @@
                         <td>{{ $row->nyha_class }}</td>
                         <td>{{ $row->type_of_acute_HF }}</td>
                         <td>{{ $row->respiratory_failure }}</td>
-                        <td>{{ $row->cardiogenic_shock }}</td>
+                        <td>{{ $row->cardiogenic_shock }}</td> --}}
                         {{-- risk Factor --}}
-                        <td>{{ $row->hypertension }}</td>
+                        {{-- <td>{{ $row->hypertension }}</td>
                         <td>{{ $row->diabetes_or_prediabetes }}</td>
                         <td>{{ $row->dislipidemia }}</td>
                         <td>{{ $row->alcohol }}</td>
@@ -171,19 +168,19 @@
                         <td>{{ $row->history_of_hf }}</td>
                         <td>{{ $row->history_of_pci_or_cabg }}</td>
                         <td>{{ $row->historyof_heart_valve_surgery }}</td>
-                        <td>{{ $row->omi_or_cad }}</td>
+                        <td>{{ $row->omi_or_cad }}</td> --}}
                         {{-- Precipitating Factors --}}
-                        <td>{{ $row->precipitating_factors }}</td>
+                        {{-- <td>{{ $row->precipitating_factors }}</td> --}}
                         {{-- Echocardiography --}}
-                        <td>{{ $row->ef }}</td>
+                        {{-- <td>{{ $row->ef }}</td>
                         <td>{{ $row->tapse }}</td>
                         <td>{{ $row->edv }}</td>
                         <td>{{ $row->esv }}</td>
                         <td>{{ $row->sign_mr }}</td>
                         <td>{{ $row->lv }}</td>
-                        <td>{{ $row->ee }}</td>
+                        <td>{{ $row->ee }}</td> --}}
                         {{-- Blood Laboratory Test --}}
-                        <td>{{ $row->hemoglobin }}</td>
+                        {{-- <td>{{ $row->hemoglobin }}</td>
                         <td>{{ $row->hematocrite }}</td>
                         <td>{{ $row->random_blood_glucose }}</td>
                         <td>{{ $row->hba1c }}</td>
@@ -194,16 +191,16 @@
                         <td>{{ $row->serum_creatinine }}</td>
                         <td>{{ $row->gfr }}</td>
                         <td>{{ $row->serum_iron }}</td>
-                        <td>{{ $row->NT_ProBNP }}</td>
+                        <td>{{ $row->NT_ProBNP }}</td> --}}
                         {{-- Blood Gas Analysis --}}
-                        <td>{{ $row->pH }}</td>
+                        {{-- <td>{{ $row->pH }}</td>
                         <td>{{ $row->pco2 }}</td>
                         <td>{{ $row->hco3 }}</td>
                         <td>{{ $row->po2 }}</td>
                         <td>{{ $row->lactate }}</td>
-                        <td>{{ $row->be }}</td>
+                        <td>{{ $row->be }}</td> --}}
                         {{-- Medication --}}
-                        <td>{{ $row->acei }}</td>
+                        {{-- <td>{{ $row->acei }}</td>
                         <td>{{ $row->aceiDoseatPredischarge }}</td>
                         <td>{{ $row->arb }}</td>
                         <td>{{ $row->arbDoseatPredischarge }}</td>
@@ -217,18 +214,18 @@
                         <td>{{ $row->Tolvaptan }}</td>
                         <td>{{ $row->statin }}</td>
                         <td>{{ $row->inotropic }}</td>
-                        <td>{{ $row->vasoconstrictor }}</td>
+                        <td>{{ $row->vasoconstrictor }}</td> --}}
                         {{-- Hospitalization --}}
-                        <td>{{ $row->iccu }}</td>
+                        {{-- <td>{{ $row->iccu }}</td>
                         <td>{{ $row->ward }}</td>
                         <td>{{ $row->totalLoS }}</td>
-                        <td>{{ $row->hospitalizationCost }}</td>
+                        <td>{{ $row->hospitalizationCost }}</td> --}}
                         {{-- Outcomes --}}
-                        <td>{{ $row->inhospitalDeath }}</td>
+                        {{-- <td>{{ $row->inhospitalDeath }}</td>
                         <td>{{ $row->vulnerablePhaseDeath }}</td>
                         <td>{{ $row->vulnerablePhaseRehospitalization }}</td>
                         <td>{{ $row->dateofDeath }}</td>
-                        <td>{!! $row->additional_notes !!}</td>
+                        <td>{!! $row->additional_notes !!}</td> --}}
                         {{-- <td>{{ $row->deleted_at }}</td> --}}
                     </tr>
                     @endforeach
